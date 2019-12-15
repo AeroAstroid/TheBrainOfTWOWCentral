0f4d2d549c624b0ed78e90835011265b0efc7607
Purplegaze <purplecoin64@gmail.com>
2019-12-15 03:12:34 -0500

    Update typingtest.py

--- Commands/typingtest.py
+++ Commands/typingtest.py
@@ -24,26 +24,33 @@ zws = "​"
 
 async def MAIN(message, args, level, perms):
 	totype = " ".join(random.sample(WORDS, random.randrange(45, 61, 1)))
-	if len(db.get_entries("typingtest", conditions={"id" : str(message.author.id)})) == 0:
-		db.add_entry("typingtest", [str(message.author.id), totype, str(message.created_at.timestamp()), "0"])
-	else:
-		db.edit_entry("typingtest", entry={"totype" : totype, "start" : str(message.created_at.timestamp())},
-			      conditions={"id" : str(message.author.id)})
 	spaced_text = ""
 	for i in range(len(totype)):
 		spaced_text += totype[i]
 		if totype[i] == " ":
 			spaced_text += zws
-	await message.channel.send(f"<@{message.author.id}>: Type these {len(totype.split(' '))} words:\n\n{spaced_text}")
+	tt = await message.channel.send(f"<@{message.author.id}>: Type these {len(totype.split(' '))} words:\n\n{spaced_text}")
+	if len(db.get_entries("typingtest", conditions={"id" : str(message.author.id)})) == 0:
+		db.add_entry("typingtest", [str(message.author.id), totype, str(tt.created_at.timestamp()), "0"])
+	else:
+		db.edit_entry("typingtest", entry={"totype" : totype, "start" : str(tt.created_at.timestamp())},
+			      conditions={"id" : str(message.author.id)})
 	msg = await BRAIN.wait_for('message', check=(lambda m: m.channel == message.channel and m.author == message.author))
 	typed_words = msg.content.lower().split(' ')
 	target_words = totype.split(' ')
 	success = 0
+	duration = msg.created_at.timestamp() - db.get_entries("typingtest", limit=50, columns=["start"], conditions={"id": message.author.id})
 	for word in typed_words:
 		if word in target_words:
 			success += 1
-			target_words.remove(word)
-	print(success)
-	print(msg.created_at.timestamp())
-		
+			for i in range(target_words.index(word)+1):
+				target_words.pop(0)
+	if success[message.author.id] < 10:
+		await message.channel.send("Typing test cancelled.")
+		return
+	else:
+		await message.channel.send(f"""<@{message.author.id}> Typing test finished!^n^n
+		Words typed correctly: {success} out of {len(totype.split(' '))} ({round(100*(success/len(totype.split(' '))), 1)}%)^n
+		Time: {duration}
+		""".replace("\n", "").replace("\t", "").replace("^n", "\n"))
 	return
