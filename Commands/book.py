b077a98562d6b403f5f3d437ed396c6070b0bce7
AeroAstroid <aeroastroid@gmail.com>
2020-04-11 17:22:08 -0300

    More convenience in trying to harvest book links

--- Commands/book.py
+++ Commands/book.py
@@ -24,11 +24,20 @@ async def MAIN(message, args, level, perms, SERVER):
 	book_names = " ".join(args[1:]).splitlines()
 
 	if perms == 3:
+		link_list = [[]]
 		for book in book_names:
 			book_image = make_book(book[:2])
 			book_image.save(f"Images/Book/{book[:2]}.png")
-			await message.channel.send(f"**{book}**", file=discord.File(f"Images/Book/{book[:2]}.png"))
+			book_msg = await message.channel.send(f"**{book}**", file=discord.File(f"Images/Book/{book[:2]}.png"))
+
+			if len(link_list[1]) + len(f"<{book_msg.attachments[0].url}>") > 1950:
+				link_list.append([])
+			link_list[-1].append(f"<{book_msg.attachments[0].url}>")
+
 			os.remove(f"Images/Book/{book[:2]}.png")
+		
+		for z in link_list:
+			await message.channel.send(z)
 	else:
 		book_image = make_book(book[0][:2])
 		book_image.save(f"Images/Book/{book[0][:2]}.png")
