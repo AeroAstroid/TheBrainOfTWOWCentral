602ff889b4810bb25281356eea36787c6f303e9f
AeroAstroid <aeroastroid@gmail.com>
2021-02-18 16:48:34 -0300

    Save/load/edit/delete newb++ programs

--- Commands/b++new.py
+++ Commands/b++new.py
@@ -4,7 +4,7 @@ from Config._bppnew_parsing import *
 
 from Config._db import Database
 
-import discord, os
+import discord, os, re, datetime, time
 
 def HELP(PREFIX):
 	return {
@@ -26,34 +26,261 @@ async def MAIN(message, args, level, perms, SERVER):
 		await message.channel.send("Include a subcommand!")
 		return
 	
+	db = Database()
+	
+	if args[1].lower() == "info":
+		tag_list = db.get_entries("b++2programs", columns=["name", "program", "author", "uses", "created", "lastused"])
+		tag_list = sorted(tag_list, reverse=True, key=lambda m: m[3])
+
+		tag_leaderboard = False
+		if level == 2: # If it's not specified, assume it's the first page
+			tag_list = tag_list[:10]
+			page = 1
+			tag_leaderboard = True
+		
+		elif is_whole(args[2]):
+			if (int(args[2]) - 1) * 10 >= len(tag_list): # Detect if the page number is too big
+				await message.channel.send(f"There is no page {args[2]} on the New B++ program list!")
+				return
+		
+			else: # This means the user specified a valid page number
+				lower = (int(args[2]) - 1) * 10
+				upper = int(args[2]) * 10
+				tag_list = tag_list[lower:upper]
+				page = int(args[2])
+				tag_leaderboard = True
+	
+		if tag_leaderboard:
+			beginning = f"```diff\nNew B++ Programs Page {page}\n\n"
+
+			for program in tag_list:
+				r = tag_list.index(program) + 1 + (page - 1) * 10
+				
+				line = f"{r}{' '*(2-len(str(r)))}: {program[0]} :: {program[3]} use{'s' if program[3] != 1 else ''}"
+
+				member_id = program[2]
+				try: # Try to gather a username from the ID
+					member = SERVER["MAIN"].get_member(int(member_id)).name
+				except: # If you can't, just display the ID
+					member = str(member_id)
+
+				created_on = datetime.utcfromtimestamp(program[4]).strftime('%Y-%m-%d %H:%M:%S UTC')
+				line += f" (written by {member} at {created_on})\n"
+			
+				beginning += line # Add this line to the final message
+			
+			beginning += "```" # Close off code block
+
+			await message.channel.send(beginning)
+			return
+
+		tag_name = args[2]
+
+		if tag_name not in [x[0] for x in tag_list]:
+			await message.channel.send("That tag does not exist.")
+			return
+		
+		program = tag_list[[x[0] for x in tag_list].index(tag_name)]
+
+		member_id = program[2]
+		try: # Try to gather a username from the ID
+			member = SERVER["MAIN"].get_member(int(member_id)).name
+		except: # If you can't, just display the ID
+			member = str(member_id)
+		
+		created_on = datetime.utcfromtimestamp(program[4]).strftime('%Y-%m-%d %H:%M:%S UTC')
+		c_d = datetime.datetime.now() - datetime.utcfromtimestamp(program[4])
+		c_d = (('' if c_d.days==0 else f'{c_d.days} day{"s" if c_d.days!=1 else ""}, ') +
+		('' if c_d.hours==0 else f'{c_d.hours} hour{"s" if c_d.hours!=1 else ""}, ') +
+		('' if c_d.minutes==0 else f'{c_d.minutes} minute{"s" if c_d.minutes!=1 else ""}, ') +
+		(f'{c_d.seconds} second{"s" if c_d.seconds!=1 else ""}'))
+		
+		msg = f"**{program[0]}** -- by {member} -- {program[3]} use{'s' if program[3]!=1 else ''}\n"
+		msg += f"Created on {created_on} `({c_d} ago)`\n"
+
+		if program[5] != 0:
+			last_used = datetime.utcfromtimestamp(program[5]).strftime('%Y-%m-%d %H:%M:%S UTC')
+			u_d = datetime.datetime.now() - datetime.utcfromtimestamp(program[5])
+			u_d = (('' if u_d.days==0 else f'{u_d.days} day{"s" if u_d.days!=1 else ""}, ') +
+			('' if u_d.hours==0 else f'{u_d.hours} hour{"s" if u_d.hours!=1 else ""}, ') +
+			('' if u_d.minutes==0 else f'{u_d.minutes} minute{"s" if u_d.minutes!=1 else ""}, ') +
+			(f'{u_d.seconds} second{"s" if u_d.seconds!=1 else ""}'))
+			msg += f"Last used on {last_used} `({u_d} ago)`\n"
+
+		if len(program[1]) > 1700:
+			msg += f"The program is too long to be included in the message, so it's in the file below:"
+			open(f'program_{program[0]}.txt', 'w', encoding="utf-8").write(program[1])
+			await message.channel.send(msg, file=discord.File(f'program_{program[0]}.txt'))
+			os.remove(f'program_{program[0]}.txt')
+		else:
+			msg += f"```{program[1]}```"
+			await message.channel.send(msg)
+		
+		return
+
+
+	if args[1].lower() == "create":
+		if level == 2:
+			await message.channel.send("Include the name of your new program!")
+			return
+	
+		tag_name = args[2]
+
+		if re.search(r"[^0-9A-Za-z_]", tag_name) or re.search(r"[0-9]", tag_name[0]):
+			await message.channel.send(
+			"Tag name can only contain letters, numbers and underscores, and cannot start with a number!")
+			return
+		
+		if tag_name in ["create", "edit", "delete", "info", "run", "help"]:
+			await message.channel.send("The tag name must not be a reserved keyword!")
+			return
+
+		if len(tag_name) > 30:
+			await message.channel.send("That tag name is too long. 30 characters maximum.")
+			return
+		
+		if level > 3:
+			program = " ".join(args[3:])
+
+		elif len(message.attachments) != 0:
+			try:
+				await message.attachments[0].save(f"Config/{message.id}.txt")
+			except Exception:
+				await message.channel.send("Include a valid program to save!")
+				return
+			
+			program = open(f"Config/{message.id}.txt", "r", encoding="utf-8").read()
+			os.remove(f"Config/{message.id}.txt")
+		
+		else:
+			await message.channel.send("Include a valid program to save!")
+			return
+		
+		while program.startswith("`") and program.endswith("`"):
+			program = program[1:-1]
+		program.replace("{}", "\t")
+
+		if (tag_name,) in db.get_entries("b++2programs", columns=["name"]):
+			await message.channel.send("There's already a program with that name!")
+			return
+		
+		db.add_entry("b++2programs", [tag_name, program, message.author.id, 0, time.time(), 0])
+		await message.channel.send(f"Successfully created program `{tag_name}`!")
+		return
+
+
+	if args[1].lower() == "edit":
+		if level == 2:
+			await message.channel.send("Include the name of the program you want to edit!")
+			return
+		
+		tag_name = args[2]
+
+		tag_list = db.get_entries("b++2programs", columns=["name", "author"])
+
+		if tag_name not in [x[0] for x in tag_list]:
+			await message.channel.send(f"There's no program under the name `{tag_name}`!")
+			return
+
+		ind = [x[0] for x in tag_list].index(tag_name)
+		if tag_list[ind][1] != str(message.author.id) and perms < 2:
+			await message.channel.send(f"You can only edit a program if you created it or if you're a staff member!")
+			return
+		
+		if level > 3:
+			program = " ".join(args[3:])
+
+		elif len(message.attachments) != 0:
+			try:
+				await message.attachments[0].save(f"Config/{message.id}.txt")
+			except Exception:
+				await message.channel.send("Include a valid program to run!")
+				return
+			
+			program = open(f"Config/{message.id}.txt", "r", encoding="utf-8").read()
+			os.remove(f"Config/{message.id}.txt")
+		
+		else:
+			await message.channel.send("Include a valid program to run!")
+			return
+		
+		while program.startswith("`") and program.endswith("`"):
+			program = program[1:-1]
+		program.replace("{}", "\t")
+		
+		db.edit_entry("b++2programs", entry={"program": program}, conditions={"name": tag_name})
+		await message.channel.send(f"Succesfully edited program {tag_name}!")
+		return
+
+
+	if args[1].lower() == "delete":
+		if level == 2:
+			await message.channel.send("Include the name of the program you want to delete!")
+			return
+		
+		tag_name = args[2]
+
+		tag_list = db.get_entries("b++2programs", columns=["name", "author"])
+
+		if tag_name not in [x[0] for x in tag_list]:
+			await message.channel.send(f"There's no program under the name `{tag_name}`!")
+			return
+
+		ind = [x[0] for x in tag_list].index(tag_name)
+		if tag_list[ind][1] != str(message.author.id) and perms < 2:
+			await message.channel.send(f"You can only edit a program if you created it or if you're a staff member!")
+			return
+			
+		db.remove_entry("b++2programs", conditions={"name": tag_name})
+		await message.channel.send(f"Succesfully deleted program {tag_name}!")
+		return
+
+
 	if args[1].lower() == "run":
-		if len(args) > 2:
+		if level > 2:
 			program = " ".join(args[2:])
 
 		elif len(message.attachments) != 0:
 			try:
 				await message.attachments[0].save(f"Config/{message.id}.txt")
 			except Exception:
-				await message.channel.send("Include a valid program!")
+				await message.channel.send("Include a valid program to run!")
 				return
 			
 			program = open(f"Config/{message.id}.txt", "r", encoding="utf-8").read()
 			os.remove(f"Config/{message.id}.txt")
-
+		
+		else:
+			await message.channel.send("Include a valid program to run!")
+			return
 
 		while program.startswith("`") and program.endswith("`"):
 			program = program[1:-1]
-		
 		program.replace("{}", "\t")
-		
-		try:
-			program_output = run_bpp_program(program).replace("<@", "<\\@")
-		except Exception as e:
-			await message.channel.send(f'{type(e).__name__}:\n```{e}```')
-			return
+	
+	else:
+		tag_name = args[1]
 
-		if len(program_output) > 1950:
-			program_output = "⚠️ `Output too long! First 1900 characters:`\n\n" + program_output[:1900]
+		tag_list = db.get_entries("b++2programs", columns=["name", "program", "uses"])
+
+		if tag_name not in [x[0] for x in tag_list]:
+			await message.channel.send(f"There's no program under the name `{tag_name}`!")
+			return
 		
-		await message.channel.send(program_output)
+		program = tag_list[1]
+
+		uses += 1
+		db.edit_entry("b++2programs", entry={"uses": uses, "lastused": time.time()}, conditions={"name": tag_name})
+
+		
+	try:
+		program_output = run_bpp_program(program).replace("<@", "<\\@")
+	except Exception as e:
+		await message.channel.send(f'{type(e).__name__}:\n```{e}```')
 		return
+
+	if len(program_output) > 1950:
+		program_output = "⚠️ `Output too long! First 1900 characters:`\n\n" + program_output[:1900]
+	
+	await message.channel.send(program_output)
+	return
