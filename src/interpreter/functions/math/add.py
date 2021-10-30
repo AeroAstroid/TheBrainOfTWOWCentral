ca093529662e77175ed916665a115a29f045daee
Xander Black <45472388+weee50@users.noreply.github.com>
2021-10-30 19:33:15 -0400

    I think this makes ADD work with arrays?

--- src/interpreter/functions/math/add.py
+++ src/interpreter/functions/math/add.py
@@ -4,6 +4,9 @@ from src.interpreter.expression import Expression
 
 
 def add(block: List, codebase):
+    if type(block[1]) == List and block[1][0] == "ARRAY":
+        block = block[1]
+    
     result = Expression(block[1], codebase)
 
     for num in block[2:]:
