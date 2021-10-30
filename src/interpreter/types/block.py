d9b7587abf9ecb1de6118ba726fd69a26be7cd12
Zolo101 <umadeguy@gmail.com>
2021-10-30 01:41:53 +0100

    Comment Obsolete Files

--- src/interpreter/types/block.py
+++ src/interpreter/types/block.py
@@ -1,38 +1,38 @@
-from typing import List, Union
-
-from src.interpreter.types.literal import Literal
-from src.interpreter.types.numeric import Numeric
-
-
-class Block:
-    def __init__(self, line: str, depth):
-        # Function name
-        self.function = line.split(" ", 1)[0][1:]
-
-        # The child blocks / Arguments
-        self.children: List[Union[Block, Literal, Numeric]] = []
-
-        # The code line itself
-        self.line = line
-
-        # Depth of block in tree
-        self.depth = 0
-
-        # Create tree
-        if self.line != "":
-            # child_blocks = "".join(self.line.split(" ", 1)[1:]).split(" ")
-            child_blocks = self.line.split(" ")
-
-            print("cb|", child_blocks)
-            # for i, child in enumerate(child_blocks):
-            #     if isBlock(child):
-            #         # If opening bracket
-            #         # If closing bracket
-            #     if isLiteral(child):
-            #         # If opening bracket
-            #         # If closing bracket
-            #     if isNumber(child):
-            # self.children.append(Number())
-
-    def debug_print_children(self):
-        return "".join(map(lambda child: child.line + " ", self.children))
+# from typing import List, Union
+#
+# from src.interpreter.types.literal import Literal
+# from src.interpreter.types.numeric import Numeric
+#
+#
+# class Block:
+#     def __init__(self, line: str, depth):
+#         # Function name
+#         self.function = line.split(" ", 1)[0][1:]
+#
+#         # The child blocks / Arguments
+#         self.children: List[Union[Block, Literal, Numeric]] = []
+#
+#         # The code line itself
+#         self.line = line
+#
+#         # Depth of block in tree
+#         self.depth = 0
+#
+#         # Create tree
+#         if self.line != "":
+#             # child_blocks = "".join(self.line.split(" ", 1)[1:]).split(" ")
+#             child_blocks = self.line.split(" ")
+#
+#             print("cb|", child_blocks)
+#             # for i, child in enumerate(child_blocks):
+#             #     if isBlock(child):
+#             #         # If opening bracket
+#             #         # If closing bracket
+#             #     if isLiteral(child):
+#             #         # If opening bracket
+#             #         # If closing bracket
+#             #     if isNumber(child):
+#             # self.children.append(Number())
+#
+#     def debug_print_children(self):
+#         return "".join(map(lambda child: child.line + " ", self.children))
--- src/interpreter/types/literal.py
+++ src/interpreter/types/literal.py
@@ -1,5 +1,5 @@
-class Literal:
-    def __init__(self):
-        self.value = ""
-        print("WIP")
-        # TODO: this is for stuff like "abc123"
+# class Literal:
+#     def __init__(self):
+#         self.value = ""
+#         print("WIP")
+#         # TODO: this is for stuff like "abc123"
