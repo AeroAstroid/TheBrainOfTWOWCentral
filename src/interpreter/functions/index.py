8c9d4ae1849e58d7e0c1ce098e7ded9827f30d3a
Zolo101 <umadeguy@gmail.com>
2021-10-31 23:15:24 +0000

    Add INDEX String support

--- src/interpreter/functions/index.py
+++ src/interpreter/functions/index.py
@@ -4,4 +4,8 @@ from src.interpreter.expression import Expression
 
 
 def index(block: List, codebase):
-    return Expression(block[1][1:][Expression(block[2], codebase)], codebase)
+    arr = Expression(block[1], codebase)
+    if type(arr) == list:
+        block[1] = block[1][1:]
+
+    return Expression(block[1][Expression(block[2], codebase)], codebase)
