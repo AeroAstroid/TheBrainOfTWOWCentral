84b01225d30c024023bba2dbdc4f052b65701876
Zolo101 <umadeguy@gmail.com>
2021-10-31 03:14:31 +0000

    Refactor Expression.py + Fix ARRAY

--- src/interpreter/expression.py
+++ src/interpreter/expression.py
@@ -1,29 +1,54 @@
-# imports -- TODO: split this
+from enum import Enum
 from typing import Union
 from typing import List
-
 
 # expression defining
 import src.interpreter.function_deco
 
 
+class Type(Enum):
+    FUNCTION = 0
+    STRING = 1
+    ARRAY = 2
+    INTEGER = 3
+    FLOAT = 4
+
+
 def Expression(block: Union[List, str], codebase):
     # TODO: try/except needed
 
-    # Literal or Numeral
-    if type(block) != list:
-        if block.isnumeric():
-            # TODO: Better way of doing this (without try/except)
-            try:
-                return int(block)
-            except ValueError:
-                return float(block)
+    blockType = isType(block)
+    if blockType == Type.FUNCTION:
+        functionWanted = src.interpreter.function_deco.functions.get(block[0])
+        print(functionWanted)
+        if functionWanted is not None:
+            return functionWanted(block, codebase)
         else:
-            return block
+            return block[0]
+    elif blockType == Type.STRING:
+        return block
+    elif blockType == Type.ARRAY:
+        array = block[1:]  # Get rid of ARRAY string
 
-    functionWanted = src.interpreter.function_deco.functions.get(block[0])
-    print(functionWanted)
-    if functionWanted is not None:
-        return functionWanted(block, codebase)
+        return list(map(lambda item: Expression(item, codebase), array))
+    elif blockType == Type.INTEGER:
+        return int(block)
+    elif blockType == Type.FLOAT:
+        return float(block)
+
+
+def isType(block):
+    if type(block) == list:
+        if block[0] == "ARRAY":
+            return Type.ARRAY
+        else:
+            return Type.FUNCTION
     else:
-        return block[0]
+        # TODO: Breaks float
+        if block.isnumeric():
+            try:
+                return Type.INTEGER
+            except ValueError:
+                return Type.FLOAT
+        else:
+            return Type.STRING
