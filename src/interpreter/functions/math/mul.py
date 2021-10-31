84a75908c9b6d52fa83fe3dfdfd8ef9401e81422
Xander Black <45472388+weee50@users.noreply.github.com>
2021-10-30 20:42:36 -0400

    Make MUL also work with arrays

--- src/interpreter/functions/math/mul.py
+++ src/interpreter/functions/math/mul.py
@@ -4,6 +4,9 @@ from src.interpreter.expression import Expression
 
 
 def mul(block: List, codebase):
+    if type(block[1]) == list and block[1][0] == "ARRAY":
+        block = block[1]
+    
     result = Expression(block[1], codebase)
 
     for num in block[2:]:
