1e3282bea5cba8ed94b171c8fefaf8c05bfec809
LegitSi <legitsi@yahoo.com>
2021-10-30 21:19:56 -0400

    potential explanation for 0.5 -> 0 bug in ROUND
    then again i dont know anything about code

--- src/interpreter/functions/round.py
+++ src/interpreter/functions/round.py
@@ -6,4 +6,5 @@ from src.interpreter.expression import Expression
 
 def round_func(block: List, codebase):
     # 0.5 gives 0 for some reason, but since B++ was the same we'll keep it
+    # in function_deco.py its returning a special formatting for "round", maybe that's the source of the issue?
     return round(Expression(block[1], codebase))
