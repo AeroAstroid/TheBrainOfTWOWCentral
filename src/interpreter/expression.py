fec8082988fade94141e15b9ae4248722e683d2d
Zolo101 <umadeguy@gmail.com>
2021-10-31 17:49:51 +0000

    Refactor isType, Fix FLOAT, negative numbers and exponents

--- src/interpreter/expression.py
+++ src/interpreter/expression.py
@@ -7,15 +7,31 @@ import src.interpreter.function_deco
 
 
 class Type(Enum):
+    # [J 100]
     FUNCTION = 0
+
+    # "Hello, World!"
     STRING = 1
+
+    # [ARRAY 1 2 3 4 5]
     ARRAY = 2
+
+    # 101
     INTEGER = 3
+
+    # 3.1415926
     FLOAT = 4
+
+    # Special Type used for the elif spam: Its a combination of float and int.
+    # 1e9, 5.3e+10
+    EXPONENT = 5
 
 
 def Expression(block: Union[List, str], codebase):
     # TODO: try/except needed
+    #[
+    if block[-1] == "":
+        block = block[:-1]
 
     blockType = isType(block)
     if blockType == Type.FUNCTION:
@@ -35,6 +51,8 @@ def Expression(block: Union[List, str], codebase):
         return int(block)
     elif blockType == Type.FLOAT:
         return float(block)
+    elif blockType == Type.EXPONENT:
+        return int(float(block))
 
 
 def isType(block):
@@ -44,11 +62,21 @@ def isType(block):
         else:
             return Type.FUNCTION
     else:
-        # TODO: Breaks float
-        if block.isnumeric():
-            try:
-                return Type.INTEGER
-            except ValueError:
-                return Type.FLOAT
+        # TODO: The bruteforce solution; Extremely inefficient
+        isFloat = False
+        isExponent = False
+        for char in block:
+            if char in ["e"]:
+                isExponent = True
+            if char in ["."]:
+                isFloat = True
+            if char not in ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "e", "+", "-", "."]:
+                return Type.STRING
+
+        if isExponent:
+            return Type.EXPONENT
+
+        if isFloat:
+            return Type.FLOAT
         else:
-            return Type.STRING
+            return Type.INTEGER
