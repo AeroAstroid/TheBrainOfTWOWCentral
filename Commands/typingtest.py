1ed9c14636a1397eff1acc7ba0fab15251af17b0
AeroAstroid // Dark <53104666+AeroAstroid@users.noreply.github.com>
2021-11-19 22:43:09 -0300

    Fix for syntax and wpm cutoff
    I feel as if 200 is already a good enough cutoff for something I would not expect anyone in the community to be able to fairly beat

--- Commands/typingtest.py
+++ Commands/typingtest.py
@@ -67,7 +67,7 @@ async def MAIN(message, args, level, perms, SERVER):
 		if success < 10 or len(typed_words) > count + 10:
 			await message.channel.send("Typing test cancelled.") # cancel if there was no attempt
 			return
-		elif wpm > 226
+		elif wpm >= 200:
 			await message.channel.send(f"Don't you think it's a bit obvious, with you going at {round(wpm, 2)} wpm?") # wr is generally said to be 216
 			return
 		else:
