bfa11c6598483be33ebcfe8ee337ea9567d9cfc4
incrementals <49661996+incrementals@users.noreply.github.com>
2021-11-23 20:42:35 -0700

    Minor update: Make Choosechar work with numbers only

--- src/interpreter/functions/choosechar.py
+++ src/interpreter/functions/choosechar.py
@@ -5,7 +5,7 @@ from src.interpreter.expression import Expression
 
 
 def choosechar(block: List, codebase):
-    choices = Expression(block[1], codebase)
+    choices = Expression(str(block[1]), codebase)
     rand = random.randint(0, len(choices) - 1)
 
     return Expression(choices[rand], codebase)
