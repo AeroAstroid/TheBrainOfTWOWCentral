487da086c07357a8893f0459cbc8669216612265
incrementals <49661996+incrementals@users.noreply.github.com>
2021-11-27 17:07:35 -0700

    Oops

--- src/interpreter/functions/replace.py
+++ src/interpreter/functions/replace.py
@@ -2,5 +2,5 @@ from typing import List
 from src.interpreter.expression import Expression
 
 
-def replace(block: List, codebase):
+def replace_func(block: List, codebase):
     return Expression(block[1], codebase).replace(Expression(block[2], codebase),Expression(block[3], codebase))
