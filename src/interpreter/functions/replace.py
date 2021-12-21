25446a5f6f6a22a17b7939005210c9b3af079b37
Zolo101 <umadeguy@gmail.com>
2021-12-21 21:53:47 +0000

    Temporary replace fix (Force type conversion)

--- src/interpreter/functions/replace.py
+++ src/interpreter/functions/replace.py
@@ -3,4 +3,4 @@ from src.interpreter.expression import Expression
 
 
 def replace_func(block: List, codebase):
-    return Expression(block[1], codebase).replace(Expression(block[2], codebase),Expression(block[3], codebase))
+    return Expression(block[1], codebase).replace(str(Expression(block[2], codebase)), str(Expression(block[3], codebase)))
