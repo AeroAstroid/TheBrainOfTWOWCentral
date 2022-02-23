0456e78e20a49509ee6cbf7f30f786b8cbea6216
Zolo101 <umadeguy@gmail.com>
2022-02-23 22:03:26 +0000

    Fix TIME

--- src/interpreter/functions/time.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-import time
-
-from src.interpreter.expression import Expression
-
-def time_func(block: List, codebase):
-    return Expression(time.time(), codebase)
---
+++ src/interpreter/functions/time_func.py
@@ -0,0 +1,6 @@
+from typing import List
+import time
+
+
+def time_func(block: List, codebase):
+    return time.time
