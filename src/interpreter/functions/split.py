ae05bb95deaa402cb092d41e4ace7115559b68d7
Zolo101 <umadeguy@gmail.com>
2022-02-23 21:55:07 +0000

    SPLIT Optional Seperator Argument

--- src/interpreter/functions/split.py
+++ src/interpreter/functions/split.py
@@ -3,4 +3,10 @@ from src.interpreter.expression import Expression
 
 
 def split(block: List, codebase):
-    return Expression(["ARRAY", *block[1]], codebase)
+    string = block[1]
+    if len(block) < 3:  # Seperator argument or not
+        string = [*string.split(" ")]
+    else:
+        string = [*string.split(block[2])]
+
+    return Expression(["ARRAY", *string], codebase)
