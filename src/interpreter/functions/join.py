b490153e924079d1c08b936c9d23317d62ce427c
Xander Black <45472388+weee50@users.noreply.github.com>
2022-02-25 21:14:22 -0500

    Revert JOIN argument adding

--- src/interpreter/functions/join.py
+++ src/interpreter/functions/join.py
@@ -5,10 +5,9 @@ from src.interpreter.expression import Expression
 
 def join(block: List, codebase):
     first = Expression(block[1], codebase)
-    delimiter = Expression(block[2], codebase) if len(block) >= 3 else ""
     
     buffer = first
     for i in block[2:]:
         buffer.extend(Expression(i, codebase))
-        buffer = delimiter.join(buffer)
+        buffer = "".join(buffer)
     return buffer
