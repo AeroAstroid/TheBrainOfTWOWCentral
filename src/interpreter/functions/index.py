fab18c387b8c7e32d1baaa753b6f138559fc034a
Zolo101 <umadeguy@gmail.com>
2022-03-01 20:28:07 +0000

    Fix INDEX

--- src/interpreter/functions/index.py
+++ src/interpreter/functions/index.py
@@ -6,6 +6,6 @@ from src.interpreter.expression import Expression
 def index(block: List, codebase):
     arr = Expression(block[1], codebase)
     if type(arr) == list:
-        block[1] = block[1][1:]
+        arr = arr[1:]
 
-    return Expression(block[1][Expression(block[2], codebase)], codebase)
+    return Expression(arr[Expression(block[2], codebase)], codebase)
