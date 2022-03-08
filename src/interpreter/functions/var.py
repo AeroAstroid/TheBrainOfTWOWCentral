e26002bfd739b23a086c00824ca9e2c4627d85f9
Zolo101 <umadeguy@gmail.com>
2022-03-08 21:39:08 +0000

    Optimize VAR index

--- src/interpreter/functions/var.py
+++ src/interpreter/functions/var.py
@@ -2,7 +2,4 @@ import src.interpreter.globals as globals
 
 
 def var(item, index):
-    if index == "":
-        return globals.codebase.variables[item]
-    else:
-        return globals.codebase.variables[item][index]
+    return globals.codebase.variables[item][index]
