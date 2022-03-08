696b529c35a46b9c1d6c97dd5288696472fb46fc
Zolo101 <umadeguy@gmail.com>
2022-03-08 22:00:33 +0000

    Revert "Optimize VAR index"
    This reverts commit e26002bfd739b23a086c00824ca9e2c4627d85f9.

--- src/interpreter/functions/var.py
+++ src/interpreter/functions/var.py
@@ -2,4 +2,7 @@ import src.interpreter.globals as globals
 
 
 def var(item, index):
-    return globals.codebase.variables[item][index]
+    if index == "":
+        return globals.codebase.variables[item]
+    else:
+        return globals.codebase.variables[item][index]
