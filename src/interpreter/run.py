65a0e49fbd2b672e19ec5f3259fbff221e277531
Zolo101 <umadeguy@gmail.com>
2022-03-09 23:51:40 +0000

    Timeouts (30 seconds)

--- src/interpreter/run.py
+++ src/interpreter/run.py
@@ -1,15 +1,14 @@
 from random import choice
 from traceback import format_exc
 
-from typing import List, Union, Dict
+from typing import List, Union
 
 import discord
+from func_timeout import func_timeout, FunctionTimedOut
 
-from src.interpreter import tempFunctionsFile
 from src.interpreter.Codebase import Codebase
 from src.interpreter.error_messages import unfunny_errmsg
 from src.interpreter.expression import Expression
-
 
 # the discord user property is used for global ownership checking
 import src.interpreter.globals as globals
@@ -18,6 +17,16 @@ from src.interpreter.tempFunctionsFile import functions
 
 
 def runCode(code: str, user: Union[discord.User, None] = None, arguments: List[str] = None):
+    try:
+        func_timeout(30, runCodeReal, args=(code, user, arguments))
+    except FunctionTimedOut:
+        return returnError("RUNTIME", "Timed out! (More than 30 seconds)")
+    except Exception as error:
+        return error
+
+
+# TODO: Find a better name for this
+def runCodeReal(code: str, user: Union[discord.User, None] = None, arguments: List[str] = None):
     # TODO: Trim up to three backticks from beginning and end of code
     parsed_code = parseCode(code)
     globals.codebase = Codebase(parsed_code, user, arguments)
@@ -25,20 +34,9 @@ def runCode(code: str, user: Union[discord.User, None] = None, arguments: List[s
 
     for statement in parsed_code:
         try:
-            if type(statement) == str:
-                result = statement
-            else:
-                result = Expression(statement, globals.codebase)
-
-            print(result)
-            if result is not None:
-                globals.codebase.output += str(result)
-
-        except Exception as e:
-            # errmsg = f"ERROR at `{statement}`:\n{e}"
-            errmsg = f"{choice(unfunny_errmsg)}\n\nERROR at `{statement}`:\n{e}"
-            print(f"{errmsg}\n\n{format_exc()}")  # print stack trace too
-            return errmsg
+            readLine(statement)
+        except Exception as error:
+            return returnError(statement, error)
 
     # print(codebase.variables)
     # print(codebase.output)
@@ -47,3 +45,20 @@ def runCode(code: str, user: Union[discord.User, None] = None, arguments: List[s
     if len(globals.codebase.output) > 2000:
         return "ERROR: Output too long!"
     return globals.codebase.output
+
+
+def readLine(statement):
+    if type(statement) == str:
+        result = statement
+    else:
+        result = Expression(statement, globals.codebase)
+
+    print(result)
+    if result is not None:
+        globals.codebase.output += str(result)
+
+
+def returnError(statement, error):
+    errmsg = f"{choice(unfunny_errmsg)}\n\nERROR at `{statement}`:\n{error}"
+    print(f"{errmsg}\n\n{format_exc()}")  # print stack trace too
+    return errmsg
