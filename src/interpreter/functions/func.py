6cca000bac2784957047e638532b833ee2e7e9e4
Zolo101 <umadeguy@gmail.com>
2022-03-10 20:09:48 +0000

    Revert FUNC infinite commit

--- src/interpreter/functions/func.py
+++ src/interpreter/functions/func.py
@@ -1,18 +1,8 @@
 from src.interpreter.userfunction import UserFunction
 
 
-def func(name, args, *code):
-    # name = Expression(block[1], codebase)
-
-    # Assume block[2] is an array (it better be)
+def func(name, args, code):
     parsed_args = {}
-
-    # Code
-    # code = block[3:]
-
-    # Stop non-blocks from being treated as blocks
-    # if len(block) == 1:
-    #     block = block[0]
 
     for i, argument in enumerate(args[1:]):
         parsed_args[i] = argument
