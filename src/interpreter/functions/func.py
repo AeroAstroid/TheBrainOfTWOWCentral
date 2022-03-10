0e2e1ad1d2a74f230a58bda5a76fd8280b490647
Zolo101 <umadeguy@gmail.com>
2022-03-10 20:01:21 +0000

    FUNC infinite code parameters

--- src/interpreter/functions/func.py
+++ src/interpreter/functions/func.py
@@ -1,7 +1,7 @@
 from src.interpreter.userfunction import UserFunction
 
 
-def func(name, args, code):
+def func(name, args, *code):
     # name = Expression(block[1], codebase)
 
     # Assume block[2] is an array (it better be)
