606595998ff7142e02535b6c25d1361e0ad2df8f
Zolo101 <umadeguy@gmail.com>
2022-03-10 22:10:34 +0000

    WHILE returning

--- src/interpreter/functions/while_func.py
+++ src/interpreter/functions/while_func.py
@@ -3,6 +3,10 @@ from src.interpreter.expression import Expression
 
 
 def while_func(condition, *code):
+    result = []
     while Expression(condition, globals.codebase):
         for line in code:
-            Expression(line, globals.codebase)
+            line_result = Expression(line, globals.codebase)
+            if line_result is not None:
+                result.append(line_result)
+    return result
