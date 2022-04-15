ffd8ee4c9333e19886f9c4d087d9d2e4c2f8aef3
Zolo101 <umadeguy@gmail.com>
2022-04-16 00:15:38 +0100

    Fix CONCAT (probably)

--- src/interpreter/functions/concat.py
+++ src/interpreter/functions/concat.py
@@ -1,23 +1,11 @@
-import src.interpreter.globals as globals
-from src.interpreter.expression import Expression
-
-
-# TODO: Refactor CONCAT
 def concat(item, *items):
-    # determine whether concat is concatenating strings or arrays
-    is_array = (type(item) == list)
-    buffer = [] if is_array else ""
+    if isinstance(item, list):
+        if not isinstance(items, tuple):
+            raise TypeError("Cannot CONCAT with mixed types")
 
-    for i in items:
-        element = Expression(i, globals.codebase)
-        if is_array != (type(element) == list):
-            raise TypeError("Cannot call CONCAT with a mix of arrays and other types")
-        if is_array:
-            buffer.extend(element)
-        else:
-            buffer += str(element)
-    
-    if is_array:
-        buffer = ''.join(map(str, buffer))
+        flatitems = [item for sublist in items for item in sublist]
+        buffer = item + flatitems
+    else:
+        buffer = item + "".join(items)
 
     return buffer
