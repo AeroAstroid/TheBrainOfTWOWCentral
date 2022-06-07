59867494a7f6bbc17cba80ee0652faae0e81512f
Inferno <49661996+infernostars@users.noreply.github.com>
2022-06-06 23:38:14 -0600

    make array single line, change name to discord name

--- src/interpreter/error_messages.py
+++ src/interpreter/error_messages.py
@@ -2,8 +2,7 @@ from random import choice
 
 
 # ive decided that these are gonna be part of the final product
-github_devs = ["Inferno", "Digin", "LegitSi", "Zelo101", "weee50",
-               "woooowoooo", "BowlingPizzaBall", "pepsi"]  # only counts people that have made a commit
+github_devs = ["Infernity", "Digin", "LegitSi", "Zelo101", "weee50", "woooowoooo", "BowlingPizzaBall", "pepsi"]  # only counts people that have made a commit
 unfunny_errmsg = [
     "GOD FUCKING DAMMIT! **crashing noises**",
     "Whoops. You broke it.",
