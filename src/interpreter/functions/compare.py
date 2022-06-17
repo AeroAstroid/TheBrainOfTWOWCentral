c200f50b62a357e0c9df937ad67cb4a49e6af444
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-06-16 22:06:29 -0700

    Bugfix

--- src/interpreter/functions/compare.py
+++ src/interpreter/functions/compare.py
@@ -7,7 +7,7 @@ def compare(v1, operator, v2):
         return v1 >= v2
     elif operator == "<=":
         return v1 <= v2
-    elif operator == "=" or "==":
+    elif operator == "=" or operator == "==":
         return v1 == v2
     elif operator == "!=":
         return v1 != v2
