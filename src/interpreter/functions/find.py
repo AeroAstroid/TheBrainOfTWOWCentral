c0206a8fca318b03e07b4c2ea7d40be7eda395c1
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-06-18 11:26:02 -0700

    Make [FIND] more useful

--- src/interpreter/functions/find.py
+++ src/interpreter/functions/find.py
@@ -1,4 +1,4 @@
-# TODO: Give better variable names
-# TODO: Make v3 and v4 optional arguments
-def find(v1, v2, v3, v4):
-    return v1.index(v2, v3, v4)
+def find(list, value):
+    if value not in list:
+        return -1
+    return list.index(value)
