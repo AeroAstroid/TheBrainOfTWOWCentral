325bb51eafd5c5173582bf065b82d10ef9669275
infernostars <blazem7708.2@gmail.com>
2022-06-20 16:59:19 -0600

    Allow getting the error type as a string in a try block

--- src/interpreter/functions/try_func.py
+++ src/interpreter/functions/try_func.py
@@ -5,5 +5,6 @@ from src.interpreter.expression import Expression
 def try_func(attempt, on_error):
     try:
         return Expression(attempt, globals.codebase)
-    except Exception:
+    except Exception as err:
+        globals.codebase.variables[0]["error"] = type(err).__name__
         return Expression(on_error, globals.codebase)
