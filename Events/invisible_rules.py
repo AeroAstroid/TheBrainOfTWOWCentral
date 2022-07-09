d4b84b13aa1585f7602ff037663dd1fe053630a0
AeroAstroid <aeroastroid@gmail.com>
2022-07-09 20:50:54 -0300

    Reset gens before each round

--- Events/invisible_rules.py
+++ Events/invisible_rules.py
@@ -393,6 +393,8 @@ class EVENT:
 		if self.GAME["ROUND"] < 0: # Ending an intermission between rounds
 			round_t = self.PARAM[f"PHASE_{self.GAME['PHASE']}_ROUND_TIME"]
 
+			self.GAME["GEN_TEST"].reset()
+
 			self.GAME["ROUND"] *= -1
 
 			self.GAME["INSPECTING"] = self.GAME["PLAYERS"].copy()
