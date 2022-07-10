1215129f48ae206ca1fbf8edf24e6ae3cc08a2a7
infernostars <blazem7708.2@gmail.com>
2022-07-10 13:37:13 -0600

    i am typing

--- src/interpreter/functions/type_func.py
+++ src/interpreter/functions/type_func.py
@@ -1,2 +1,5 @@
+import src.interpreter.globals as globals
+from src.interpreter.expression import Expression
+
 def type_func(a): # i unno what to name the var
-	return type(a).__name__
+	return type(Expression(a, globals.codebase)).__name__
