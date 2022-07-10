fc36a21bd5b1ba04746fbe7ebcac6b941651aeaf
infernostars <blazem7708.2@gmail.com>
2022-07-10 13:31:55 -0600

    i tried making map and failed rip

--- src/interpreter/functions/arrays/map_func.py
+++ src/interpreter/functions/arrays/map_func.py
@@ -3,4 +3,5 @@ from src.interpreter.expression import Expression
 
 
 def map_func(function, listToEdit):
-    return map(Expression(function, globals.codebase), listToEdit)
+    print(list(map(lambda f: Expression(function, globals.codebase), listToEdit)))
+    return map(lambda f: Expression(function, globals.codebase), listToEdit)
