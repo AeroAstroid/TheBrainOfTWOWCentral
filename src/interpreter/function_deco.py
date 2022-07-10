ac557fe12fa27a8d81130937009cf1383609620b
infernostars <blazem7708.2@gmail.com>
2022-07-10 13:01:40 -0600

    arrays i forgor

--- src/interpreter/function_deco.py
+++ src/interpreter/function_deco.py
@@ -40,7 +40,7 @@ from src.interpreter.functions.block import block
 from src.interpreter.functions.return_func import return_func
 from src.interpreter.functions.raise_func import raise_func
 
-from src.interpreter.functions.array.map_func import map_func
+from src.interpreter.functions.arrays.map_func import map_func
 from src.interpreter.functions.arrays.max_func import max_func
 from src.interpreter.functions.arrays.min_func import min_func
 from src.interpreter.functions.arrays.randomizer import randomizer
