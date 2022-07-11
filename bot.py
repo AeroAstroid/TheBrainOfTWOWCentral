72b87543a04ff776fc43def10d6e18567a428593
infernostars <blazem7708.2@gmail.com>
2022-07-10 20:34:06 -0600

    fix 2

--- bot.py
+++ bot.py
@@ -4,3 +4,7 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix="b/", description="B* bot!!", intents=intents)
+
+async def postrun(output):
+    await ctx.send(output['main'])
+    await ctx.author.dm_channel.send(output['pm'])
