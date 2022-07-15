fb4cef23310f9eec02f374d0c464a06f446d4d3a
infernostars <blazem7708.2@gmail.com>
2022-07-15 16:08:03 -0600

    parser

--- src/interpreter/parse.py
+++ src/interpreter/parse.py
@@ -10,24 +10,26 @@ start: bstar*
     | ("true" | "True") -> true
     | ("false" | "False") -> false
     | string
-    | SIGNED_INT -> integer
-    | DECIMAL -> float
+    | integer
+    | float
     | function
     | array
     | unescaped_string
 string: ESCAPED_STRING
 block: ALPHANUMERIC
+float.-0: DECIMAL
+integer.-1: SIGNED_INT
 array: "{" [arg ("," arg)*] "}"
 function: ("[") (block | function) arg* ("]")
 COMMENT: ("[# ") ALLBUTBRACKETS ("]")
-unescaped_string: ALPHANUMERIC
+unescaped_string.-3: ALPHANUMERIC
 ALLBUTBRACKETS: ALLEXCEPTBRACKETS+
 DIGIT: "0".."9"
 LCASE_LETTER: "a".."z"
 UCASE_LETTER: "A".."Z"
 LETTER: UCASE_LETTER | LCASE_LETTER
 ALPHANUMERIC: (ALLNONCONFLICTING)+
-ALLNONCONFLICTING: /([^\[\]0-9\{\}\"\s\,\-]|\- )/
+ALLNONCONFLICTING: /([^\[\]\{\}\"\s\,\-]|\- )/
 ALLEXCEPTBRACKETS: /[^\[\]]/
 // imports from common library my beloved
 %import common.ESCAPED_STRING
