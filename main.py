5df531f02bb50cac3e9d0c45a99a8b28734361f9
infernostars <blazem7708.2@gmail.com>
2022-07-15 20:58:53 -0600

    await

--- main.py
+++ main.py
@@ -32,7 +32,7 @@ async def on_ready():
 async def accept_file_or_message(ctx, message):
     if len(ctx.message.attachments) > 0:
         attachment = ctx.message.attachments[0]
-        file = attachment.open().decode("utf-8")
+        file = await attachment.open().decode("utf-8")
         print 
         if attachment.size >= 150_000:
             raise "File is too large! (150KB MAX)"
