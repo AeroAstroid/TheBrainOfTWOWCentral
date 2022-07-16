e25a98994942808c16de5d642eee232d61b378e1
infernostars <blazem7708.2@gmail.com>
2022-07-15 22:37:20 -0600

    aye eye

--- src/interpreter/functions/range_func.py
+++ src/interpreter/functions/range_func.py
@@ -1,18 +1,10 @@
 def range_func(index_start, index_end, index_step):
 
-    if index_step is None:
-        index_step = 1
-    else:
-        index_step = int(index_step)
-    
-    if index_end is None or index_end is "":
-        index_end = ""
-    else:
-        index_end = int(index_end)
-
+    index_step = 1 if index_step is None else int(index_step)
+    index_end = "" if index_end is None or index_end is "" else int(index_end)
     if index_start is None or index_start is "":
         index_start = ""
     else:
         index_start = int(index_start)
 
-    return range(index_start, index_end, index_step)
+    return list(range(index_start, index_end, index_step))
