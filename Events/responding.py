dd755e652373e0f3a43a0b9c7931b44b3b71bf8e
NeonicPlasma <43083380+NeonicPlasma@users.noreply.github.com>
2022-07-19 20:54:37 +1200

    Fixing zero width space issue

--- Events/responding.py
+++ Events/responding.py
@@ -700,7 +700,7 @@ class EVENT:
 	async def response_valid(self, original_response, channel):
 
 		# Run the formatting_fix function, remove characters that might cause issues and strip unnecessary whitespace
-		response = formatting_fix(original_response.replace("`", "").replace("\t", "").replace("\n", "").replace("\u200b")).strip()
+		response = formatting_fix(original_response.replace("`", "").replace("\t", "").replace("\n", "").replace("\u200b", "")).strip()
 
 		# Convert response to UTF-8 only characters
 		response = response.encode('UTF-8', 'ignore').decode("UTF-8")
