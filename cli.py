62952e1e8d6fad747dd34a2aeb0496746c0b939a
infernostars <blazem7708.2@gmail.com>
2022-08-10 15:39:01 -0600

    Better Command Line [1.0.2]

--- cli.py
+++ cli.py
@@ -1,22 +1,43 @@
+#!/usr/bin/env python3
+# above is for shells that support shebangs
+from importlib.resources import path
 from src.interpreter.function_deco import setupFunctions
 from src.interpreter.globals import debug
 from src.interpreter.run import runCodeSandbox
 from tests import Colours
+import argparse, pathlib
 
+argparser = argparse.ArgumentParser(description='Run B* code in the terminal')
+exclgroup = argparser.add_mutually_exclusive_group()
+exclgroup.add_argument("-n", "--interactive",dest="interactive", action="store_true", help="Enable interactive/shell mode. Use CTRL+C to exit.")
+exclgroup.add_argument("-i", "--input", help="Take in an input file at INPUT.", type=pathlib.Path)
+argparser.add_argument("-o", "--output", help="Put the output into a file at OUTPUT. Only works if you are taking in a file.", type=pathlib.Path)
+argparser.add_argument('args', nargs="*")
+
+parsed = argparser.parse_args()
 empty = ""
 debug.print_debug = False
 setupFunctions()
-print("------------------------")
-print(Colours.OKGREEN + Colours.BOLD + "B* CLI" + Colours.ENDC)
-print(Colours.OKBLUE + Colours.BOLD + "press enter for a new line, and press enter twice to run the code.\n\n" + Colours.ENDC)
+if parsed.interactive:
+    while True:
+        print(Colours.BOLD + "enter your b* code:\n" + Colours.ENDC)
 
-while True:
-    print(Colours.BOLD + "enter your b* code:\n" + Colours.ENDC)
-
-    code = "\n".join(iter(input, empty))
-    if code.startswith("file "):
-        code = code.removeprefix("file ")
-        with open(code) as f:
-            code = f.read()
-    result = runCodeSandbox(code)
-    print("Result below:\n\n", result, "\n", sep="")
+        code = "\n".join(iter(input, empty))
+        if code.startswith("file "):
+            code = code.removeprefix("file ")
+            with open(code) as f:
+                code = f.read()
+        result = runCodeSandbox(code)
+        print("Result below:\n\n", result, "\n", sep="")
+else:
+    if parsed.input:
+        with parsed.input.open() as file:
+            text = file.read()
+            file.close()
+        result = runCodeSandbox(text, None, parsed.args)
+    if parsed.output:
+        with parsed.output.open("w") as file:
+            text = file.write(result)
+            file.close()
+    else:
+        print(result)
