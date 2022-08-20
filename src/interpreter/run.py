a2a5c022b90e0e8899212ce474529752685d3104
infernostars <blazem7708.2@gmail.com>
2022-08-19 20:10:44 -0600

    what if...  embed

--- src/interpreter/run.py
+++ src/interpreter/run.py
@@ -44,10 +44,10 @@ def runCodeSandbox(code: Tree, user: Union[discord.User, None] = None, arguments
     # print(codebase.variables)
     # print(codebase.output)
     if len(globals.codebase.output) == 0 or globals.codebase.output.isspace():
-        return "⚠️: The code has ran successfully, but returned nothing!"
+        return discord.Embed(description="⚠️: The code has ran successfully, but returned nothing!")
     if len(globals.codebase.output) > 4000:
-        return f"⚠️: Output too long, only showing the first 3800 characters:\n\n```{globals.codebase.output[:3800]}```"
-    return globals.codebase.output
+        return discord.Embed(description=f"⚠️: Output too long, only showing the first 3800 characters:\n\n```{globals.codebase.output[:3800]}```")
+    return discord.Embed(description=globals.codebase.output)
 
 
 def readLine(statement):
@@ -70,4 +70,4 @@ def returnError(statement, error, i):
              f"```\n**{error}** (Error occurred at code block {i + 1})"
     if globals.debug.print_error:
         print(f"{errmsg}\n\n{format_exc()}")  # print stack trace too
-    return errmsg
+    return discord.Embed(description=errmsg)
