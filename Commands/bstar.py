bf1ff5fc3e3f2e64c1d7d064dda0e166c0568058
Zolo101 <umadeguy@gmail.com>
2022-12-27 06:59:24 +0000

    Re-Implement Database

--- Commands/bstar.py
+++ Commands/bstar.py
@@ -1,12 +1,16 @@
 import os
+import re
+import time
 
 import discord
 
 from Commands.b_star.interpreter.function_deco import setupFunctions
 from Commands.b_star.interpreter.run import runCode
 
-from Commands.b_star.database.s3 import getTag, updateTag, createTag, infoTag, leaderboards, isOwnerProgram, editTag, \
-	deleteTag
+from Config._functions import is_whole
+from Config._db import Database
+
+from datetime import datetime as dt
 
 
 def HELP(PREFIX):
@@ -28,7 +32,7 @@ def HELP(PREFIX):
 	}
 
 
-PERMS = 1 # Member
+PERMS = 1  # Member
 ALIASES = ["B*", "BSTAR"]
 REQ = ["LOGIN"]
 
@@ -57,91 +61,288 @@ async def MAIN(message, args, level, perms, SERVER, LOGIN):
 		await message.channel.send("Include a subcommand!")
 		return
 
+	db = Database()
 	subcommand = args[1].lower()
 
-	# @bot.command()
-	# async def run(ctx, *, message=None):
 	# """Run B* code"""
 	if subcommand == "run":
+		# TODO: Does this need to be replaced with b++.py version?
 		try:
 			output = runCode(await accept_file_or_message(message), message.author)
 			await message.channel.send(embed=discord.Embed(description=output))
 		except Exception as e:
 			await message.channel.send(e)
+		return
 
-
-	# @bot.command()
-	# async def tag(ctx, message, *, args=""):
-	# """Runs a B* tag"""
-	if subcommand == "tag":
-		tagObject = getTag(message)
-		if tagObject is not None:
-			code = tagObject["program"]
-			# TODO: this is float only rn, do support for int in the future
-			argument_list = args.split(" ")
-
-			output = runCode(code, message.author, argument_list)
-			await message.channel.send(output)
-
-			# If all goes well, then increment the use
-			updateTag(message)
-		else:
-			await message.channel.send(f"There's no program under the name `{message}`!")
-
-
-	# @bot.command()
-	# async def create(ctx, name, *, message=None):
 	# """Creates a B* tag with your code"""
 	if subcommand == "create":
-		name = args[2]
-		# try:
-		if len(name) < 50:
+		if level == 2:
+			await message.channel.send("Include the name of your new program!")
+			return
+
+		tag_name = args[2]
+
+		if re.search(r"[^0-9A-Za-z_]", tag_name) or re.search(r"[0-9]", tag_name[0]):
+			await message.channel.send(
+				"Tag name can only contain letters, numbers and underscores, and cannot start with a number!")
+			return
+
+		if tag_name in ["create", "edit", "delete", "info", "run", "help"]:
+			await message.channel.send("The tag name must not be a reserved keyword!")
+			return
+
+		if len(tag_name) > 30:
+			await message.channel.send("That tag name is too long. 30 characters maximum.")
+			return
+
+		if level > 3:
+			program = " ".join(args[3:])
+
+		elif len(message.attachments) != 0:
 			try:
-				createTag(message.author, name, await accept_file_or_message(message))
-				await message.channel.send(f"Tag `{name}` created!")
-			except Exception as e:
-				await message.channel.send(e)
+				if message.attachments[0].size >= 60000:
+					await message.channel.send("Your program must be under **60KB**.")
+					return
+
+				await message.attachments[0].save(f"Config/{message.id}.txt")
+
+			except Exception:
+				await message.channel.send("Include a valid program to save!")
+				return
+
+			program = open(f"Config/{message.id}.txt", "r", encoding="utf-8").read()
+			os.remove(f"Config/{message.id}.txt")
+
 		else:
-			await message.channel.send(f"The name \"`{name}`\" is too long!")
-		# except:
-		#     await message.channel.send("Tag creation failed")
+			await message.channel.send("Include a valid program to save!")
+			return
 
+		while program.startswith("`") and program.endswith("`"):
+			program = program[1:-1]
+		program.replace("{}", "\t")
 
-	# @bot.command()
-	# async def info(ctx, message):
+		if (tag_name,) in db.get_entries("bsprograms", columns=["name"]):
+			await message.channel.send("There's already a program with that name!")
+			return
+
+		# db.add_entry("bsprograms", [0, int(time.time()), 0, tag_name, program, message.author.id])
+		db.add_entry("bsprograms", [tag_name, program, message.author.id, 0, int(time.time()), 0])
+		await message.channel.send(f"Successfully created program `{tag_name}`!")
+		return
+
+	# except:
+	#     await message.channel.send("Tag creation failed")
+
 	# """Gives infomation and source code about a B* tag"""
 	if subcommand == "info":
-		await message.channel.send(await infoTag(message))
+		tag_list = db.get_entries("bsprograms", columns=["name", "program", "author", "uses", "created", "lastused"])
+		tag_list = sorted(tag_list, reverse=True, key=lambda m: m[3])
 
+		tag_leaderboard = False
+		if level == 2:  # If it's not specified, assume it's the first page
+			tag_list = tag_list[:10]
+			page = 1
+			tag_leaderboard = True
 
-	# @bot.command()
-	# async def leaderboard(ctx, page: int = 0):
+		elif is_whole(args[2]):
+			if (int(args[2]) - 1) * 10 >= len(tag_list):  # Detect if the page number is too big
+				await message.channel.send(f"There is no page {args[2]} on the New B++ program list!")
+				return
+
+			else:  # This means the user specified a valid page number
+				lower = (int(args[2]) - 1) * 10
+				upper = int(args[2]) * 10
+				tag_list = tag_list[lower:upper]
+				page = int(args[2])
+				tag_leaderboard = True
+
+		if tag_leaderboard:
+			beginning = f"```scala\nB++ Programs Page {page}\n\n"
+
+			for program in tag_list:
+				r = tag_list.index(program) + 1 + (page - 1) * 10
+
+				line = f"{r}{' ' * (2 - len(str(r)))}: {program[0]} :: {program[3]} use{'s' if program[3] != 1 else ''}"
+
+				member_id = program[2]
+				try:  # Try to gather a username from the ID
+					member = SERVER["MAIN"].get_member(int(member_id)).name
+				except:  # If you can't, just display the ID
+					member = str(member_id)
+
+				created_on = dt.utcfromtimestamp(program[4]).strftime('%Y-%m-%d %H:%M:%S UTC')
+				line += f" (written by {member} at {created_on})\n"
+
+				beginning += line  # Add this line to the final message
+
+			beginning += "```"  # Close off code block
+
+			await message.channel.send(beginning)
+			return
+
+		tag_name = args[2]
+
+		if tag_name not in [x[0] for x in tag_list]:
+			await message.channel.send("That tag does not exist.")
+			return
+
+		program = tag_list[[x[0] for x in tag_list].index(tag_name)]
+
+		member_id = program[2]
+		try:  # Try to gather a username from the ID
+			member = SERVER["MAIN"].get_member(int(member_id)).name
+		except:  # If you can't, just display the ID
+			member = str(member_id)
+
+		created_on = dt.utcfromtimestamp(program[4]).strftime('%Y-%m-%d %H:%M:%S UTC')
+		c_d = dt.now() - dt.utcfromtimestamp(program[4])
+
+		d = c_d.days
+		h, rm = divmod(c_d.seconds, 3600)
+		m, s = divmod(rm, 60)
+
+		c_d = (('' if d == 0 else f'{d} day{"s" if d != 1 else ""}, ') +
+			   ('' if h == 0 else f'{h} hour{"s" if h != 1 else ""}, ') +
+			   ('' if m == 0 else f'{m} minute{"s" if m != 1 else ""}, ') +
+			   (f'{s} second{"s" if s != 1 else ""}'))
+
+		msg = f"**{program[0]}** -- by {member} -- {program[3]} use{'s' if program[3] != 1 else ''}\n"
+		msg += f"Created on {created_on} `({c_d} ago)`\n"
+
+		if program[5] != 0:
+			last_used = dt.utcfromtimestamp(program[5]).strftime('%Y-%m-%d %H:%M:%S UTC')
+			u_d = dt.now() - dt.utcfromtimestamp(program[5])
+
+			d = u_d.days
+			h, rm = divmod(u_d.seconds, 3600)
+			m, s = divmod(rm, 60)
+
+			u_d = (('' if d == 0 else f'{d} day{"s" if d != 1 else ""}, ') +
+				   ('' if h == 0 else f'{h} hour{"s" if h != 1 else ""}, ') +
+				   ('' if m == 0 else f'{m} minute{"s" if m != 1 else ""}, ') +
+				   (f'{s} second{"s" if s != 1 else ""}'))
+
+			msg += f"Last used on {last_used} `({u_d} ago)`\n"
+
+		if len(program[1]) > 1700:
+			msg += f"The program is too long to be included in the message, so it's in the file below:"
+			open(f'program_{program[0]}.txt', 'w', encoding="utf-8").write(program[1])
+			await message.channel.send(msg, file=discord.File(f'program_{program[0]}.txt'))
+			os.remove(f'program_{program[0]}.txt')
+		else:
+			msg += f"```{program[1]}```"
+			await message.channel.send(msg)
+
+		return
+
 	# 	"""Shows the leaderboard of tags sorted by uses"""
-	if subcommand == "leaderboard":
-		page = int(args[2])
-		await message.channel.send(await leaderboards(page))
+	# if subcommand == "leaderboard":
+	# 	page = int(args[2])
+	# 	await message.channel.send(await leaderboards(page))
 
-
-	# @bot.command()
-	# async def edit(ctx, name, *, message=None):
 	# 	"""Edit one of your B* tags"""
 	if subcommand == "edit":
-		if isOwnerProgram(name, message.author.id):
+		if level == 2:
+			await message.channel.send("Include the name of the program you want to edit!")
+			return
+
+		tag_name = args[2]
+
+		tag_list = db.get_entries("bsprograms", columns=["name", "author"])
+
+		if tag_name not in [x[0] for x in tag_list]:
+			await message.channel.send(f"There's no program under the name `{tag_name}`!")
+			return
+
+		ind = [x[0] for x in tag_list].index(tag_name)
+		if tag_list[ind][1] != str(message.author.id) and perms < 2:
+			await message.channel.send(f"You can only edit a program if you created it or if you're a staff member!")
+			return
+
+		if level > 3:
+			program = " ".join(args[3:])
+
+		elif len(message.attachments) != 0:
 			try:
-				editTag(name, await accept_file_or_message(message))
-				await message.channel.send(f"Tag `{name}` edited!")
-			except Exception as e:
-				await message.channel.send(e)
+				if message.attachments[0].size >= 60000:
+					await message.channel.send("Your program must be under **60KB**.")
+					return
+
+				await message.attachments[0].save(f"Config/{message.id}.txt")
+
+			except Exception:
+				await message.channel.send("Include a valid program to run!")
+				return
+
+			program = open(f"Config/{message.id}.txt", "r", encoding="utf-8").read()
+			os.remove(f"Config/{message.id}.txt")
+
 		else:
-			await message.channel.send(f"You aren't the owner of tag `{name}`!")
+			await message.channel.send("Include a valid program to run!")
+			return
 
+		while program.startswith("`") and program.endswith("`"):
+			program = program[1:-1]
 
-	# @bot.command()
-	# async def delete(ctx, name):
+		program = program.replace("{}", "\v")
+
+		db.edit_entry("bsprograms", entry={"program": program}, conditions={"name": tag_name})
+		await message.channel.send(f"Succesfully edited program {tag_name}!")
+		return
+
 	# 	"""Delete one of your B* tags"""
 	if subcommand == "delete":
-		if isOwnerProgram(name, message.author.id):
-			deleteTag(name)
-			await message.channel.send(f"Tag `{name}` deleted!")
-		else:
-			await message.channel.send(f"You aren't the owner of tag `{name}`!")
+		if level == 2:
+			await message.channel.send("Include the name of the program you want to delete!")
+			return
+
+		tag_name = args[2]
+
+		tag_list = db.get_entries("bsprograms", columns=["name", "author"])
+
+		if tag_name not in [x[0] for x in tag_list]:
+			await message.channel.send(f"There's no program under the name `{tag_name}`!")
+			return
+
+		ind = [x[0] for x in tag_list].index(tag_name)
+		if tag_list[ind][1] != str(message.author.id) and perms < 2:
+			await message.channel.send(f"You can only edit a program if you created it or if you're a staff member!")
+			return
+
+		db.remove_entry("bsprograms", conditions={"name": tag_name})
+		await message.channel.send(f"Succesfully deleted program {tag_name}!")
+		return
+
+	# Find the tag
+	# tagObject = getTag(message)
+	# if tagObject is not None:
+	#     code = tagObject["program"]
+	#     # TODO: this is float only rn, do support for int in the future
+	#     argument_list = args.split(" ")
+	#
+	#     output = runCode(code, message.author, argument_list)
+	#     await message.channel.send(output)
+	#
+	#     # If all goes well, then increment the use
+	#     updateTag(message)
+	# else:
+	#     await message.channel.send(f"There's no program under the name `{message}`!")
+
+	tag_name = args[1]
+
+	tag_list = db.get_entries("bsprograms", columns=["name", "program", "author", "uses"])
+
+	if tag_name not in [x[0] for x in tag_list]:
+		await message.channel.send(f"There's no program under the name `{tag_name}`!")
+		return
+
+	tag_info = [x for x in tag_list if x[0] == tag_name][0]
+	program = tag_info[1]
+
+	uses = tag_info[3] + 1
+	db.edit_entry("bsprograms", entry={"uses": uses, "lastused": time.time()}, conditions={"name": tag_name})
+
+	program_args = args[2:]
+
+	output = runCode(program, message.author, program_args)
+	await message.channel.send(output)
