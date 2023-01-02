b021bbe4aefb47807acfc09dca90130d8d8678bb
UnseenSevet <59371356+UnseenSevet@users.noreply.github.com>
2023-01-01 22:03:56 -0600

    give me more power!!!
    making the command more powerful by allowing you to use some of cexprtk's control structure processing

--- Commands/math.py
+++ Commands/math.py
@@ -23,13 +23,15 @@ async def MAIN(message, args, level, perms, SERVER):
 	try:
 		st = cexprtk.Symbol_Table(variables={"e": math.e, "phi": (1 + math.sqrt(5))/2},add_constants=True, functions={"rand":random.uniform})
 		e = cexprtk.Expression(" ".join(args[1:]), st)
-		output = e()
+		output = e.value()
 		if output % 1 == 0: output = int(output)
 		else: output = round(output,15)
-			
-		await message.channel.send(embed=discord.Embed(title=f"Expression result:", description=str(output)[:100]))
+		
+		if math.isnan(output) and len(e.results()) != 0: output = str(e.results()
+		
+		await message.channel.send(embed=discord.Embed(title=f"Expression result:", description=str(output)[:1000]))
 	except Exception as e:
-		await message.channel.send("Something went wrong. Please try again.")
+		await message.channel.send("There was an error in parsing your statement.")
 		raise e
 	return
   
