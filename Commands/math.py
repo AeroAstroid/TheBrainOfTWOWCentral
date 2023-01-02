3a0fe1fa56e8ce8d79210c356e6cd5ccc0d6ee3f
UnseenSevet <59371356+UnseenSevet@users.noreply.github.com>
2023-01-01 23:27:22 -0600

    codeblock support

--- Commands/math.py
+++ Commands/math.py
@@ -24,8 +24,12 @@ async def MAIN(message, args, level, perms, SERVER):
 		return
 	
 	try:
+		expression_string = " ".join(args[1:])
+		while expression_string.startswith("`") and expression_string.endswith("`"):
+			expression_string = expression_string[1:-1]
+			
 		st = cexprtk.Symbol_Table(variables={"e": math.e, "phi": (1 + math.sqrt(5))/2},add_constants=True, functions={"rand":random.uniform})
-		e = cexprtk.Expression(" ".join(args[1:]), st)
+		e = cexprtk.Expression(expression_string, st)
 		output = e.value()
 		if output % 1 == 0: output = int(output)
 		else: output = round(output,15)
