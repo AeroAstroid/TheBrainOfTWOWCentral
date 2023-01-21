faa6fceecf9d69428cc300a1f2df40b15e9988ac
Inferno <blazem7708.2@gmail.com>
2023-01-20 22:12:45 -0700

    Update user_func.py

--- Config/b_star_interpreter/functions/user_func.py
+++ Config/b_star_interpreter/functions/user_func.py
@@ -18,7 +18,13 @@ def user_func(userItemToGet):
             return datetime.timestamp(globals.codebase.user.created_at)
         case "display_name":
             return globals.codebase.user.display_name
+        case "color":
+            return globals.codebase.user.color
+        case "accent_color":
+            return globals.codebase.user.accent_color
         case "":
-            return [globals.codebase.user.name, globals.codebase.user.id, globals.codebase.user.discriminator, str(globals.codebase.user.avatar.url), datetime.timestamp(globals.codebase.user.created_at) globals.codebase.user.display_name]
+            return [globals.codebase.user.name, globals.codebase.user.id, globals.codebase.user.discriminator,
+                    str(globals.codebase.user.avatar.url), datetime.timestamp(globals.codebase.user.created_at),
+                    globals.codebase.user.display_name, globals.codebase.user.color, globals.codebase.user.accent_color]
         case _:
-            raise NotImplementedError("Currently, user only supports `name`, `id`, `discriminator`, `avatar`, `created_at`, and `display_name`.")
+            raise NotImplementedError("Currently, user only supports `name`, `id`, `discriminator`, `avatar`, `created_at`, `display_name`, `color`, and `accent_color`.")
