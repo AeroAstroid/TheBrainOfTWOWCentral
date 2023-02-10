55fea748ea73e89076e45355e18738d212cf6b25
Inferno <infernity@infernity.dev>
2023-02-10 10:58:14 -0700

    Make tc/tag link use embeds

--- Commands/link.py
+++ Commands/link.py
@@ -1,5 +1,6 @@
 from Config._links import LINKS
 from Config._functions import grammar_list
+from discord import Embed
 
 def HELP(PREFIX):
 	return {
@@ -20,13 +21,13 @@ async def MAIN(message, args, level, perms, SERVER):
 	# Note: this takes data from Config/_links.py, which is where I store all link info
 
 	if level == 1: # If it's just `tc/l`, provide a list of the links available
-		await message.channel.send(f"Here's a list of link commands available:\n\n{grammar_list(list(LINKS.keys()))}")
+		await message.channel.send(embed=Embed(color=0x6168d4,description=f"Here's a list of link commands available:\n\n{grammar_list(list(LINKS.keys()))}"))
 		return
 
 	if args[1].upper() not in LINKS.keys(): # The link does not exist
-		await message.channel.send("That link cannot be found.")
+		await message.channel.send(embed=Embed(color=0xFF0000,description="That link cannot be found."))
 		return
 	
 	requested = args[1].upper() # Provide the link and info on it
-	await message.channel.send(f"{LINKS[requested]['INFO']}\n\n**Link :** {LINKS[requested]['MAIN']}")
+	await message.channel.send(embed=Embed(color=0x61e643,description=f"[{LINKS[requested]['INFO']}]({LINKS[requested]['MAIN']})")) # the [bla](domain) should make a link
 	return
