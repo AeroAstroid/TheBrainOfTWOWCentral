8c228d229444517a968e6c4956bed17155848f42
Zolo101 <umadeguy@gmail.com>
2023-04-25 23:38:38 +0100

    rewrite COMPARE

--- Config/b_star_interpreter/functions/compare.py
+++ Config/b_star_interpreter/functions/compare.py
@@ -1,20 +1,19 @@
 # TODO: Inspect types further
-def comparevals(v1: float, operator: str, v2: float):
-    if operator == ">":
-        return v1 > v2
-    elif operator == "<":
-        return v1 < v2
-    elif operator == ">=":
-        return v1 >= v2
-    elif operator == "<=":
-        return v1 <= v2
-    elif operator == "=" or operator == "==":
-        return v1 == v2
-    elif operator == "!=":
-        return v1 != v2
-
-
 def compare(v1: float, operator: str, v2: float):
-    if comparevals(v1, operator, v2):
-        return 1
-    return 0
+    match operator:
+        case ">":
+            result = v1 > v2
+        case "<":
+            result = v1 < v2
+        case ">=":
+            result = v1 >= v2
+        case "<=":
+            result = v1 <= v2
+        case "=" | "==":
+            result = v1 == v2
+        case "!=":
+            result = v1 != v2
+        case _:
+            raise NotImplementedError(f"operator '{operator}' not implemented")
+
+    return int(result)
