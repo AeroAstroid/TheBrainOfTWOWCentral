aa5d320c2db3268ea8432e9439703753beefcc79
Clover <59371356+cloverpepsi@users.noreply.github.com>
2024-05-14 20:15:40 -0500

    where in the world is tc/tag sandiego part 2
    (add channelid returning thing in the parser)

--- Config/_bpp_parsing.py
+++ Config/_bpp_parsing.py
@@ -238,6 +238,9 @@ def run_bpp_program(code, p_args, author, runner):
 			
 			elif result[0] == "aa":
 				result = p_args
+
+			elif result[0] == "c_id":
+				result = runner.channel.id
 		
 		functions[k] = result
 		return result
